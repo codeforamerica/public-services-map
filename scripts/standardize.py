e565e8a10f208bd448fc268153abc11a66fae71d
Richa Agarwal <richa.agarwal85@gmail.com>
2013-01-19 17:11:48 -0800

    Adding sample data files

--- scripts/standardize.py
+++ scripts/standardize.py
@@ -1,14 +1,17 @@
 import csv
+import json
+
 # example usage: 
-# output_to_csv("/public-services-map/data/OaklandUnifiedSchools.csv","/public-services-map/data/output.csv", indices = [1, 2, 3])
+# from standardize import output_to_csv
+# output_to_csv("/Users/richa/cfa/public-services-map/data/OaklandUnifiedSchools.csv","/Users/richa/cfa/public-services-map/data/output.csv", indices = [1, 3, 2], headers = ["type", "name", "address"])
 
 def clean(string):
 	return string.strip("\"")
 
 def the_basics(data, indices = None, delimiter = ","):
 	data.next()
-	basics = []
 	for row in data:
+		basics = []
 		row = row.split(delimiter)
 		if indices:
 			for index in indices:
@@ -17,8 +20,9 @@ def the_basics(data, indices = None, delimiter = ","):
 			basics.append(clean(row[0]))
 		yield basics
 
-def output_to_csv(file_in, file_out, indices = None):
-	outfile = csv.writer(open(file_out, "wb"))
+def output_to_csv(file_in, file_out, indices = None, headers = None):
+	outfile = csv.writer(open(file_out, "w"))
+	outfile.writerow(headers)
 	for record in the_basics(open(file_in, "r"), indices):
 		outfile.writerow(record)
 
